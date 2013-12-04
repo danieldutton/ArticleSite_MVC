27e4b6d84131d2c877586c9b9cb6fa3a4e2d0048
Daniel Dutton <danielbdutton@googlemail.com>
2013-12-04 22:18:45 +0000

    Skeleton DbContext and DataInit class added to Data layer

---
+++ ArticleSite.DataAccess/ArticleDbContent.cs
@@ -0,0 +1,15 @@
+﻿using ArticleSite.Model.Entities;
+using System.Data.Entity;
+
+namespace ArticleSite.DataAccess
+{
+    public class ArticleDbContent : DbContext
+    {
+        DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
