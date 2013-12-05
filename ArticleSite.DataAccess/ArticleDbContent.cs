046629039cd95c27ecabb5c669a29303a3063222
Daniel Dutton <danielbdutton@googlemail.com>
2013-12-05 10:14:29 +0000

    Created Final DbContext and Data Initialiser

--- ArticleSite.DataAccess/ArticleDbContent.cs
+++ ArticleSite.DataAccess/ArticleDbContent.cs
@@ -5,10 +5,22 @@ namespace ArticleSite.DataAccess
 {
     public class ArticleDbContent : DbContext
     {
-        DbSet<Article> Articles { get; set; }
+        public DbSet<Article> Articles { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+        public ArticleDbContent() : base("ArticleDb")
+        {            
+        }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Article>()
+                         .HasMany(j => j.Categories)
+                         .WithMany(j => j.Articles)
+                         .Map(x => x.ToTable("ArticleCategory"));
+
             base.OnModelCreating(modelBuilder);
         }
     }
--- ArticleSite.DataAccess/DataInitialiser.cs
+++ ArticleSite.DataAccess/DataInitialiser.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using ArticleSite.Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArticleSite.DataAccess
 {
     public class DataInitialiser : DropCreateDatabaseAlways<ArticleDbContent>
     {
+        protected override void Seed(ArticleDbContent context)
+        {
+            var articles = new List<Article>
+                {
+                    new Article { DatePublished = new DateTime(2011, 12, 2), Title = "Title 1",  Content = "Content 1", Categories = new List<Category> {new Category {Name = "Category 1"}}},
+                    new Article { DatePublished = new DateTime(2011, 12, 2), Title = "Title 1",  Content = "Content 1", Categories = new List<Category> {new Category {Name = "Category 1"}}},
+                    new Article { DatePublished = new DateTime(2011, 12, 2), Title = "Title 1",  Content = "Content 1", Categories = new List<Category> {new Category {Name = "Category 1"}}},
+                    new Article { DatePublished = new DateTime(2011, 12, 2), Title = "Title 1",  Content = "Content 1", Categories = new List<Category> {new Category {Name = "Category 1"}}},
+                    new Article { DatePublished = new DateTime(2011, 12, 2), Title = "Title 1",  Content = "Content 1", Categories = new List<Category> {new Category {Name = "Category 1"}}},
+                };
+
+            foreach (var article in articles)
+            {
+                context.Articles.Add(article);
+            }
+
+            base.Seed(context);
+        }
     }
 }
